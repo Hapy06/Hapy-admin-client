18d35203d3892b2cc8e198a410a4e36101b82f43
msgassama <gassamasanoussy15@gmail.com>
2023-03-01 17:27:18 +0000

    modification design poste preparation

--- src/components/HapyButtonWithIcon_Little.tsx
+++ src/components/HapyButtonWithIcon_Little.tsx
@@ -4,14 +4,19 @@ type PropsType = {
     handleClick: any ;
     iconComponent:any ;
     btnWidth?:number;
+    btnHeight?:number;
     btnClass?: string ;
 }
 
 function HapyButtonWithIcon_Little(props:PropsType) {
     return (
         <>
-            <button className={'hapy-btn-with-icon vertical-center ' + props.btnClass} onClick={props.handleClick} style={{width:props.btnWidth}}>
-                <span className="float-start" style={{marginLeft:8, marginRight:8}} >{props.iconComponent}</span>
+            <button className={'hapy-btn-with-icon vertical-center ' + props.btnClass} onClick={props.handleClick} 
+                style={{
+                    ...(props.btnWidth && {width: props.btnWidth}),
+                    ...(props.btnHeight && {width: props.btnHeight}),
+                }}>
+                <span style={{marginLeft:'auto', marginRight:'auto'}} >{props.iconComponent}</span>
             </button>
         </>
     )
